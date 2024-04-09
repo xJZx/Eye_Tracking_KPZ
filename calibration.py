3cb5bda6c29d925484fdb6fd6db2da0f9d88616e
Kacper Krzesinski <k.krzesinski02@gmail.com>
2024-04-09 18:36:02 +0200

    Fixed drawing green circle after pressing space

--- calibration.py
+++ calibration.py
@@ -16,33 +16,48 @@ class CalibrationApp:
         self.screen_height = self.master.winfo_screenheight()
 
         self.radius = 10
+        self.actual_circle = 0
 
         self.master.bind('<Key>', self.toggle_rendering)
+
+        self.circle_coords = [
+            (0, 0),  # Top left corner
+            (self.screen_width / 2 - self.radius, 0),  # Middle of top edge
+            (self.screen_width - self.radius * 2, 0),  # Top right corner
+            (0, self.screen_height / 2 - self.radius),  # Middle of left edge
+            (self.screen_width / 2 - self.radius, self.screen_height / 2 - self.radius),  # Middle of the screen
+            (self.screen_width - self.radius * 2, self.screen_height / 2 - self.radius),  # Middle of right edge
+            (0, self.screen_height - self.radius * 2),  # Bottom left corner
+            (self.screen_width / 2 - self.radius, self.screen_height - self.radius * 2),  # Middle of bottom edge
+            (self.screen_width - self.radius * 2, self.screen_height - self.radius * 2)  # Bottom right corner
+        ]
 
         self.draw_calibration_points()
 
     def draw_calibration_points(self):
-        circle_coords = [
-            (0, 0),  # Top left corner
-            (self.screen_width - self.radius * 2, 0),  # Top right corner
-            (0, self.screen_height - self.radius * 2),  # Bottom left corner
-            (self.screen_width - self.radius * 2, self.screen_height - self.radius * 2),  # Bottom right corner
-            (self.screen_width / 2 - self.radius, 0),  # Middle of top edge
-            (self.screen_width - self.radius * 2, self.screen_height / 2 - self.radius),  # Middle of right edge
-            (0, self.screen_height / 2 - self.radius),  # Middle of left edge
-            (self.screen_width / 2 - self.radius, self.screen_height - self.radius * 2),  # Middle of bottom edge
-            (self.screen_width / 2 - self.radius, self.screen_height / 2 - self.radius)  # Middle of the screen
-        ]
-
-        # Create circles and store their object IDs
-        for x, y in circle_coords:
-            circle = self.canvas.create_oval(x, y, x + self.radius * 2, y + self.radius * 2, fill='red', outline='red')
+        for x, y in self.circle_coords:
+            if x == 0 and y == 0:
+                circle = self.canvas.create_oval(x, y, x + self.radius * 2, y + self.radius * 2, fill='green',
+                                                 outline='green')
+            else:
+                circle = self.canvas.create_oval(x, y, x + self.radius * 2, y + self.radius * 2, fill='red',
+                                                 outline='red')
 
     def draw_calibration(self):
-        pass
+        i = 0
+        for x, y in self.circle_coords:
+            if i == self.actual_circle:
+                circle = self.canvas.create_oval(x, y, x + self.radius * 2, y + self.radius * 2, fill='green',
+                                                 outline='green')
+            else:
+                circle = self.canvas.create_oval(x, y, x + self.radius * 2, y + self.radius * 2, fill='red',
+                                                 outline='red')
+            i += 1
 
     def toggle_rendering(self, event):
-        pass
+        if event.keysym == 'space':
+            self.actual_circle += 1
+            self.draw_calibration()
 
 
 def start_app():

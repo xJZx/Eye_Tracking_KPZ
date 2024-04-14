b4817145adb4d999be6b4496718acf5f9d279fa1
xJZx <jakubsuski10@gmail.com>
2024-04-14 19:32:52 +0200

    Now it is possible to determine, when the eye is looking at the screen. Added borders showing when the eye is not looking at the screen.

--- pupil_detection.py
+++ pupil_detection.py
@@ -40,20 +40,26 @@ def start_eye_tracking():
         contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
-        for cnt in contours:
-            (x, y, w, h) = cv2.boundingRect(cnt)
-            #cv2.drawContours(frame, [cnt], -1, (0, 0, 255), 3)
-            cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
-            cv2.line(roi, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
-            break
-
         # (x1, y1), ((x1 + (x2 - x1), (y1 + (y2 - y1))        (x2 - x1) = width        (y2 - y1) = height
         (x1, y1) = saved_coordinates[2]  # bo lustrzane odbicie w kamerze
         (x2, y2) = saved_coordinates[6]  # bo lustrzane odbicie w kamerze
         # print(type(x2), type(x1))
         # print(x2, x1)
         cv2.rectangle(roi, (x2, y2), (x1, y1), (0, 0, 255), 2)
+
+        x_middle = 0
+        y_middle = 0
+
+        for cnt in contours:
+            (x, y, w, h) = cv2.boundingRect(cnt)
+            x_middle = int(x + (w / 2))
+            y_middle = int(y + (h / 2))
+            # cv2.drawContours(frame, [cnt], -1, (0, 0, 255), 3)
+            if x1 < x_middle < x2 and y1 < y_middle < y2:
+                cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.line(roi, (x + int(w / 2), 0), (x + int(w / 2), rows), (0, 255, 0), 2)
+                cv2.line(roi, (0, y + int(h / 2)), (cols, y + int(h / 2)), (0, 255, 0), 2)
+            break
 
         cv2.imshow("threshold", threshold)
         cv2.imshow("gray frame", gray_frame)
